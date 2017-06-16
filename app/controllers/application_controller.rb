e538438c67d7197233e1cd55d21b17012f65b074
pawelztef <pawelztef@gmail.com>
2017-06-16 12:47:46 +0100

    settings confirmable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
   protected
   def configure_premitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :remeber_me])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
