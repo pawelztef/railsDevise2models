318bf1cca94b945e9a967b41193bc593913bf4dd
pawelztef <pawelztef@gmail.com>
2017-06-19 14:38:15 +0100

    helper method to depic pending invitation status

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,7 +1,9 @@
 module AdminsHelper
   def admin_status(admin)
-    if current_account.owner == admin
-      content_tag(:span, '', class: 'check color-success')
+    if current_admin.owner
+      material_icon.done.css_class('text-success')
+    else
+      'Oczekuje'
     end
   end
 end
