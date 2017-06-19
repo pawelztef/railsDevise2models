de1937684ced22af47df019054beaad9c66e7ffc
pawelztef <pawelztef@gmail.com>
2017-06-19 11:43:34 +0100

    tlumaczenie wersja podstawowa

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,13 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :phone, presence: true
+  validates :phone, numericality: { only_integer: true }
+  validates :address_line1, presence: true
+  validates :address_line2, presence: true
+  validates :code, presence: true
+  validates :town, presence: true
+
 end
